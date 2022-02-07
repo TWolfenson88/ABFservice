292c4f1b5b4d789ee9dccfc606b64e5fab72aae4
Tawr <see.fill@yandex.ru>
2022-02-07 18:34:27 +0300

    add service init struct
    time for api integration

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/TWolfenson88/ABFservice/core"
+	"github.com/TWolfenson88/ABFservice/core/wblists"
+)
+
+type ABFConfig struct {
+	BucketConfig core.BucketConfig
+	BucketStorage core.BucketStorage
+	NetLists  wblists.NetLists
+}
+
+func InitABF() *ABFConfig {
+	return &ABFConfig{
+		BucketConfig:  core.BucketConfig{},
+		BucketStorage: core.BucketStorage{},
+		NetLists:      wblists.NetLists{},
+	}
+}
 
 func main() {
 	fmt.Println("Hello test")
+
+	//заинитить необходимые структуры
+	
+	ABFcfg := InitABF()
+
+
+	//постучаться в бд
+	//получить конфиги на лимитер
+	//получить чернобелые листы
+	//закинуть данные в структуры
+	//сесть слушать порт
+
+	
 }
